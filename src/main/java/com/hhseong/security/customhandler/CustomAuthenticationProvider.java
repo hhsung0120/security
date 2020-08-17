37d127998840fe56b5bd312a6049f22ca6365756
hshan <hhsung0120@naver.com>
2020-08-17 22:54:38 +0900

    시큐리티 설정

--- src/main/java/com/hhseong/security/customhandler/CustomAuthenticationProvider.java
+++ src/main/java/com/hhseong/security/customhandler/CustomAuthenticationProvider.java
@@ -1,22 +1,34 @@
 package com.hhseong.security.customhandler;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationManager {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String user_id = (String)authentication.getPrincipal();
+        String userId = (String)authentication.getPrincipal();
+        String userPassword = (String)authentication.getCredentials();
+        System.out.println(userId);
+        System.out.println(userPassword);
 
-        String user_pw = (String)authentication.getCredentials();
+        //로그인 실제 성공 여부 로직 실행
 
-        System.out.println(user_id);
-        System.out.println(user_pw);
+        List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
+        roles.add(new SimpleGrantedAuthority("ROLE_USER"));
 
-        System.out.println(1);
-        return null;
+        UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(userId, userPassword, roles);
+        return result;
     }
+
+
 }
